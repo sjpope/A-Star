65d31fa8963102a176d12996457861fda0935a97
Samuel J Pope <spope@inrsenterprises.com>
2024-11-16 05:37:57 -0600

    Add initial configuration and setup files for C++ development

---
+++ setup.cpp
@@ -0,0 +1,17 @@
+// g++ -o setup.exe setup.cpp
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    vector<vector<int>> v = {{1, 2, 3}, {8, 0, 4}, {7, 6, 5}};
+
+    vector<vector<int>> init = {{2, 8, 3}, {1, 6, 4}, {0, 7, 5}};
+    vector<vector<int>> init2 = {{2, 1, 6}, {4, 0, 8}, {7, 5, 3}};
+
+
+    return 0;
+}
